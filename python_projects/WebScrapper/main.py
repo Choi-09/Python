b7861e92689413c103a09e7827f6d716a088dc77
vanessajchoi <vanessajchoi@gmail.com>
2022-12-12 19:07:13 +0900

    도메인, 검색, 파일저장 기능 구현

---
+++ python_projects/WebScrapper/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, send_file
+from job_scrappers.rmok import extract_jobs
+from job_scrappers.wwr import extract_wwr_jobs
+from file import save_to_file
+
+app = Flask("JobScrapper")
+
+db = {}  
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", name='stranger')
+
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword == "":
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        rmok = extract_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = rmok + wwr
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect('/')
+
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
+
+
+app.run("0.0.0.0")
